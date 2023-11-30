1721368889012e8c4621e49ca1024d18983f3cab
Pham Bao <baobaoo5533@gmail.com>
2023-11-30 14:08:28 +0700

    add tailwind css

---
+++ pages/thecoffeetree_html/assets/js/app.js
@@ -0,0 +1,7 @@
+// app.js
+
+import "tailwindcss/base";
+import "tailwindcss/components";
+import "tailwindcss/utilities";
+
+/* Bổ sung mã JavaScript của bạn sau đó (nếu cần) */
---
+++ pages/thecoffeetree_html/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Example content paths...
+    "../**/*.html",
+    "../../**/*.{js,jsx,ts,tsx,vue}",
+  ],
+  separator: "_",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
